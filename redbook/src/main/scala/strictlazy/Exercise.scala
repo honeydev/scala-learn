40c37109f4bc8440ea847fc4b55d122ac721e28f
honeydev <aleksey.lebedev@protonmail.com>
2024-07-02 01:25:15 +1000

    Add inf streams exercises

--- redbook/src/main/scala/strictlazy/Exercise.scala
+++ redbook/src/main/scala/strictlazy/Exercise.scala
@@ -1,4 +1,6 @@
 package strictlazy
+
+
 sealed trait Stream[+A] {
 
   def toList: List[A] =
@@ -56,11 +58,10 @@ sealed trait Stream[+A] {
 
   def merge[B >: A](b: => Stream[B]): Stream[B] =
     foldRight(b)((a, b) => Stream.cons(a, b))
-  
+
   def flatMap[B](f: A => Stream[B]) =
     foldRight[Stream[B]](Empty)((a, acc) => f(a) `merge` acc)
 }
-
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
@@ -76,6 +77,34 @@ object Stream {
 
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+  def constant[A](a: A): Stream[A] =
+    Stream.cons(a, constant(a))
+
+  def from(n: Int): Stream[Int] =
+    Stream.cons(n, from(n + 1))
+
+  def fibs: Stream[Int] = {
+    def iter(a: Int, b: Int): Stream[Int] =
+      Stream.cons(a, iter(b, a + b))
+    iter(0, 1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z) match {
+      case Some((x, y)) => Stream.cons(x, unfold(y)(f))
+      case None         => Empty
+    }
+  }
+
+  def constant2[A](n: A): Stream[A] =
+    Stream.unfold(n)(_ => Some((n, n)))
+
+  def from2(n: Int): Stream[Int] =
+    Stream.unfold(n)(s => Some((s, s + 1)))
+
+  def fibs2: Stream[Int] =
+    Stream.unfold((0, 1)) { case (a, b) => Some((a, (b, a + b))) }
 }
 
 object Exercise extends App {
@@ -94,5 +123,18 @@ object Exercise extends App {
   println(s.append(4).toList)
   println(s.merge(Stream(4, 5, 6)).toList)
   println(Stream(Stream(1), Stream(2)).flatMap(x => x).toList)
+  LazyList(1, 2, 3).map { v =>
+    println(s"first f with $v")
+    v + 1
+  }.map { v =>
+    println(s"second f with $v")
+    v - 1
+  }.toList
+  println(Stream.constant(1).take(5).toList)
+  println(Stream.from(1).take(10).toList)
+  println(Stream.fibs.take(10).toList)
+  println(Stream.unfold(0)(s => Some((s, s + 1))).take(4).toList)
+  println(Stream.constant2(3).take(6).toList)
+  println(Stream.from2(1).take(10).toList)
+  println(Stream.fibs2.take(10).toList)
 }
-
